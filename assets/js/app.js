aebc8c9b2f5d5827d62316c9b3d48872f100975c
Juan Miramontes <juan.miramontes@accenture.com>
2019-01-16 17:51:55 -0600

    Removing jQuery

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var app = {
+  init: function () {
+    document.getElementById('app').textContent = 'Hello from Typescript!!';
+  }
+};
+app.init();
--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,6 +1,6 @@
 var app = {
   init: function () {
-    document.getElementById('app').textContent = 'Hello from Typescript';
+    document.getElementById('app').textContent = 'Hello from Typescript, ¿Que pues?';
   }
 };
 
